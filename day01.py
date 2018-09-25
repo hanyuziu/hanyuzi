b4f09b32bc754f29e4909431669f7dde12c26836
root <root@localhost.localdomain>
2018-09-25 07:07:15 -0400

    1

--- day01.py
+++ day01.py
@@ -1,1 +1,9 @@
-print("hello world!")
+# EP:2
+x,y,a,b,c = 1,1,1,0,0
+part1 = (3 + 4 * x) / 5
+part2 = 10 * (y - 5) * (a + b + c) / x
+part3 = 9 * ((4 / x) + (9 + x)/y)
+
+res = part1 - part2  + part3
+print(res)
+
